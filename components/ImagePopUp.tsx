cda1002484c930fff37547bb419559393b1f1238
Felix s1910238061 <s2210629018@fhooe.at>
2023-06-11 15:52:24 +0200

    added fade in and fade out of close button

--- components/ImagePopUp.tsx
+++ components/ImagePopUp.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 
 interface Props {
@@ -7,20 +7,77 @@ interface Props {
 }
 
 const ImagePopUp: React.FC<Props> = ({ onClose, image }) => {
+  const [showCloseButton, setShowCloseButton] = useState(false);
+
+  useEffect(() => {
+    let timeoutId: NodeJS.Timeout | null = null;
+
+    const handleMouseMove = () => {
+      setShowCloseButton(true);
+      clearTimeout(timeoutId!);
+      timeoutId = setTimeout(() => {
+        setShowCloseButton(false);
+      }, 1000);
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+      clearTimeout(timeoutId!);
+    };
+  }, []);
+
   const handleClick = () => {
     onClose();
+  };
+
+  const handleImageClick = (event: React.MouseEvent<HTMLImageElement>) => {
+    event.stopPropagation();
   };
 
   return (
     <div className="backdrop" onClick={handleClick}>
       <div className="imageContainer">
-          {/* check if image or if base64 */}
-          {image.includes("data:image") ? (
-            <img src={image} alt="Full-screen image" className="fullscreenImage" />
-          ) : (
-            <Image src={image} alt="Full-screen image" className="fullscreenImage" width={512} height={512}/>
-          )}
-        <div className="closeButtonContainer">
+        {/* check if image or if base64 */}
+        {image.includes("data:image") ? (
+          <div className="imageContainerWrapper">
+            <Image
+              src={image}
+              alt="Full-screen image"
+              className="fullscreenImage"
+              width={512}
+              height={512}
+              onClick={handleImageClick}
+            />
+            {/* if showCloseButton true, show topGradient and black box */}
+            {showCloseButton && (
+              <>
+                <div className="topGradient" />
+                <div className="blackBox" />
+              </>
+            )}
+          </div>
+        ) : (
+          <div className="imageContainerWrapper">
+            <Image
+              src={image}
+              alt="Full-screen image"
+              className="fullscreenImage"
+              width={512}
+              height={512}
+              onClick={handleImageClick}
+            />
+            {/* if showCloseButton true, show topGradient and black box */}
+            {showCloseButton && (
+              <>
+                <div className="topGradient" />
+                <div className="blackBox" />
+              </>
+            )}
+          </div>
+        )}
+        <div className={`closeButtonContainer ${showCloseButton ? "fadeIn" : ""}`}>
           <button className="closeButton" onClick={handleClick}>
             <span className="closeSymbol">✕</span>
           </button>
